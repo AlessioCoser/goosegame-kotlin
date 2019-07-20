5010885f316ce9d1c7634aa27533484f5740d0c6
Alessio Coser <alessio.coser@xpeppers.com>
2019-07-20 10:58:20 +0200

    refactor by policies

--- src/main/kotlin/com/xpeppers/goosegame/GooseGame.kt
+++ src/main/kotlin/com/xpeppers/goosegame/GooseGame.kt
@@ -11,40 +11,14 @@ fun main() {
 class GooseGame(private val players: Players, private val diceRoller: DiceRoller) {
     private val winSpace = 63
 
+    private val bridgeSpace = 6
+
     fun execute(command: String): String {
         if (command.startsWith("move ")) {
             return movePlayer(command)
         }
 
         return addPlayers(command)
-    }
-
-    private fun movePlayer(command: String): String {
-        val elements = command.split(" ", ",")
-        val player = players.find(elements[1])
-        val dice = diceRoller.roll()
-
-        val previousPosition = player.position
-        players.updatePosition(player, previousPosition + dice.sum)
-
-        if (player.position > winSpace) {
-            players.updatePosition(player, winSpace - (player.position - winSpace))
-            return printMovePlayer(player.name, dice, previousPosition, winSpace) +
-                    ". ${player.name} bounces! ${player.name} returns to ${player.position}"
-        }
-
-        if (player.position == winSpace) {
-            return printMovePlayer(player.name, dice, previousPosition, player.position) +
-                    ". ${player.name} Wins!!"
-        }
-
-        if (player.position == 6) {
-            players.updatePosition(player, 12)
-            return printMovePlayer(player.name, dice, previousPosition, 6) +
-                    ". ${player.name} jumps to 12"
-        }
-
-        return printMovePlayer(player.name, dice, previousPosition, player.position)
     }
 
     private fun addPlayers(command: String): String {
@@ -59,6 +33,40 @@ class GooseGame(private val players: Players, private val diceRoller: DiceRoller
         return "players: " + players.names().joinToString(", ")
     }
 
+    private fun movePlayer(command: String): String {
+        val elements = command.split(" ", ",")
+        val player = players.find(elements[1])
+        val dice = diceRoller.roll()
+
+        val previousPosition = player.position
+        players.updatePosition(player, previousPosition + dice.sum)
+
+        if (isWinPolicy(player)) {
+            players.updatePosition(player, winSpace - (player.position - winSpace))
+            return printMovePlayer(player.name, dice, previousPosition, winSpace) +
+                    ". ${player.name} bounces! ${player.name} returns to ${player.position}"
+        }
+
+        if (isBouncePolicy(player)) {
+            return printMovePlayer(player.name, dice, previousPosition, player.position) +
+                    ". ${player.name} Wins!!"
+        }
+
+        if (isBridgePolicy(player)) {
+            players.updatePosition(player, 12)
+            return printMovePlayer(player.name, dice, previousPosition, bridgeSpace) +
+                    ". ${player.name} jumps to 12"
+        }
+
+        return printMovePlayer(player.name, dice, previousPosition, player.position)
+    }
+
+    private fun isBridgePolicy(player: Player) = player.position == bridgeSpace
+
+    private fun isBouncePolicy(player: Player) = player.position == winSpace
+
+    private fun isWinPolicy(player: Player) = player.position > winSpace
+
     private fun printMovePlayer(name: String, dice: Dice, previousPosition: Int, newPosition: Int): String {
         return "$name rolls ${dice.first}, ${dice.second}. $name moves from ${printPosition(previousPosition)} to ${printPosition(newPosition)}"
     }
@@ -68,7 +76,7 @@ class GooseGame(private val players: Players, private val diceRoller: DiceRoller
             return "Start"
         }
 
-        if (position == 6) {
+        if (position == bridgeSpace) {
             return "The Bridge"
         }
 
