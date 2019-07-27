5c768f8ff6a7cf9bf0a6ff51bfe61155713929b5
Alessio Coser <alessio.coser@xpeppers.com>
2019-07-27 16:33:01 +0200

    extracts move private methods in HttpPrinter

--- src/main/kotlin/com/xpeppers/goosegame/HttpPrinter.kt
+++ src/main/kotlin/com/xpeppers/goosegame/HttpPrinter.kt
@@ -1,41 +1,35 @@
 package com.xpeppers.goosegame
 
 class HttpPrinter : Printer {
-    override fun prank(name: String, beginPosition: Int, landPosition: Int, dice: Dice, other: Player): GameResponse {
-        return GameResponse.ok("{\"$name\":{\"rolls\":[${dice.first},${dice.second}],\"moves\":{\"from\":\"${printPosition(beginPosition)}\",\"to\":\"${printPosition(landPosition)}\"},\"status\":\"${other.name} returns to ${other.position}!\"}}")
-    }
+    override fun prank(name: String, beginPosition: Int, landPosition: Int, dice: Dice, other: Player) =
+        GameResponse.ok(move(name, dice, beginPosition, landPosition, "${other.name} returns to ${other.position}!"))
 
-    override fun theGoose(name: String, beginPosition: Int, landPosition: Int, dice: Dice, moves: MutableList<Int>): GameResponse {
-        return GameResponse.ok("{\"$name\":{\"rolls\":[${dice.first},${dice.second}],\"moves\":{\"from\":\"${printPosition(beginPosition)}\",\"to\":\"${printPosition(moves.last())}\"},\"status\":\"Moves again!\"}}")
-    }
+    override fun theGoose(name: String, beginPosition: Int, landPosition: Int, dice: Dice, moves: MutableList<Int>) =
+        GameResponse.ok(move(name, dice, beginPosition, moves.last(), "Moves again!"))
 
-    override fun bridge(player: Player, dice: Dice, previousPosition: Int, bridgeSpace: Int): GameResponse {
-        return GameResponse.ok("{\"${player.name}\":{\"rolls\":[${dice.first},${dice.second}],\"moves\":{\"from\":\"${printPosition(previousPosition)}\",\"to\":\"${printPosition(player.position)}\"},\"status\":\"Jumps!\"}}")
-    }
+    override fun bridge(player: Player, dice: Dice, previousPosition: Int, bridgeSpace: Int) =
+        GameResponse.ok(move(player.name, dice, previousPosition, player.position, "Jumps!"))
 
-    override fun win(player: Player, dice: Dice, previousPosition: Int): GameResponse {
-        return GameResponse.ok("{\"${player.name}\":{\"rolls\":[${dice.first},${dice.second}],\"moves\":{\"from\":\"${printPosition(previousPosition)}\",\"to\":\"${printPosition(player.position)}\"},\"status\":\"Wins!\"}}")
-    }
+    override fun win(player: Player, dice: Dice, previousPosition: Int) =
+        GameResponse.ok(move(player.name, dice, previousPosition, player.position, "Wins!"))
 
-    override fun bounce(player: Player, dice: Dice, previousPosition: Int, winSpace: Int): GameResponse {
-        return GameResponse.ok("{\"${player.name}\":{\"rolls\":[${dice.first},${dice.second}],\"moves\":{\"from\":\"${printPosition(previousPosition)}\",\"to\":\"${printPosition(player.position)}\"},\"status\":\"Bounces!\"}}")
-    }
+    override fun bounce(player: Player, dice: Dice, previousPosition: Int, winSpace: Int) =
+        GameResponse.ok(move(player.name, dice, previousPosition, player.position, "Bounces!"))
 
-    override fun movePlayer(name: String, dice: Dice, previousPosition: Int, newPosition: Int): GameResponse {
-        return GameResponse.ok("{\"$name\":{\"rolls\":[${dice.first},${dice.second}],\"moves\":{\"from\":\"${printPosition(previousPosition)}\",\"to\":\"${printPosition(newPosition)}\"},\"status\":\"\"}}")
-    }
+    override fun movePlayer(name: String, dice: Dice, previousPosition: Int, newPosition: Int) =
+        GameResponse.ok(move(name, dice, previousPosition, newPosition))
 
-    override fun playerAlreadyExists(playerName: String): GameResponse {
-        return GameResponse.alreadyExists("{\"alreadyExists\": \"$playerName: Already existing player\"}")
-    }
+    override fun playerAlreadyExists(playerName: String) =
+        GameResponse.alreadyExists("{\"alreadyExists\": \"$playerName: Already existing player\"}")
 
-    override fun players(names: List<String>): GameResponse {
-        return GameResponse.ok("{\"players\": [\"${names.joinToString("\", \"")}\"]}")
-    }
+    override fun players(names: List<String>) =
+        GameResponse.ok("{\"players\": [\"${names.joinToString("\", \"")}\"]}")
 
-    override fun notFound(): GameResponse {
-        return GameResponse.notFound("")
-    }
+    override fun notFound() =
+        GameResponse.notFound("")
+
+    private fun move(name: String, dice: Dice, previousPosition: Int, currentPosition: Int, status: String = "") =
+        "{\"$name\":{\"rolls\":[${dice.first},${dice.second}],\"moves\":{\"from\":\"${printPosition(previousPosition)}\",\"to\":\"${printPosition(currentPosition)}\"},\"status\":\"$status\"}}"
 
     private fun printPosition(position: Int): String = when (position) {
         0 -> "Start"
